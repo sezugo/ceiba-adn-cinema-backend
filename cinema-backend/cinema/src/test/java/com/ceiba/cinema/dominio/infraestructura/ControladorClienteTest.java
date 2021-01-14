108a401c0e782052ec19f36ef76f21b8b85e5d40
Sebastián Zuluaga Gómez - Ceiba Software <sebastian.zuluaga@ceiba.com.co>
2021-01-13 21:35:54 -0500

    Test de integracion al controlador

--- cinema-backend/cinema/src/test/java/com/ceiba/cinema/dominio/infraestructura/ControladorClienteTest.java
+++ cinema-backend/cinema/src/test/java/com/ceiba/cinema/dominio/infraestructura/ControladorClienteTest.java
@@ -5,6 +5,7 @@ import com.ceiba.cinema.CinemaApplication;
 import com.ceiba.cinema.aplicacion.comando.ComandoCliente;
 import com.ceiba.cinema.testdatabuilder.ComandoClienteTestDataBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
@@ -18,7 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class ControladorClienteTest {
 
+    private static final String CEDULA = "123";
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -42,6 +44,7 @@ public class ControladorClienteTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
     }
 
+
     @Test
     public void crearCliente() throws Exception{
         //Arrange
@@ -51,8 +54,31 @@ public class ControladorClienteTest {
         mockMvc.perform(post("/cliente")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(comandoCliente)))
-                .andExpect(status().isOk()
-                );
+                .andExpect(status().isOk())
+                .andDo(a-> {
+                    Assert.assertEquals(comandoCliente.getIdCliente().longValue(), 10);
+                    Assert.assertEquals(comandoCliente.getNombre(), "Peter");
+                    Assert.assertEquals(comandoCliente.getApellido(), "Parker");
+                    Assert.assertEquals(comandoCliente.getCedula(), "1036400700");
+                    Assert.assertEquals(comandoCliente.getCelular(), "3135811244");
+                    Assert.assertEquals(comandoCliente.getCorreo(), "peterparker@marvel.com");
+                });
+    }
+
+    @Test
+    public void eliminarCliente() throws Exception {
+        //Arrange
+        ComandoCliente comandoCliente = new ComandoClienteTestDataBuilder().conCedula("123").build();
+
+        //Act - Assert
+        mockMvc.perform(post("/cliente")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(comandoCliente)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(delete("/cliente/".concat(CEDULA))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
 }
